fbe63081ec53b0a0118fc1c62a61755a6fef4811
miguelsanchezp <miguelsanchez1703@gmail.com>
2020-10-23 16:17:32 +0200

    Local script to upload files from one folder

---
+++ backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+find /home/username/folder/ -newermt `date +%F -d today` >> /home/user/.Backup/files.txt;
+sed -i -e 's/\/home\/user\///g' /home/user/.Backup/files.txt;
+rsync -au --files-from=/home/user/.Backup/files.txt /home/user/ host:/home/user/.Backup/;
+rm /home/user/.Backup/files.txt;
+
+
