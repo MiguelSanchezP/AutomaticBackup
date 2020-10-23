1600b05d1b03f4dd2cb1b5b28515dc24a4b5c43a
Miguel Sánchez Palomino <miguelsanchez1703@gmail.com>
2020-10-23 16:19:47 +0200

    change username for user

--- Local/backup.sh
+++ Local/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find /home/username/folder/ -newermt `date +%F -d today` >> /home/user/.Backup/files.txt;
+find /home/user/folder/ -newermt `date +%F -d today` >> /home/user/.Backup/files.txt;
 sed -i -e 's/\/home\/user\///g' /home/user/.Backup/files.txt;
 rsync -au --files-from=/home/user/.Backup/files.txt /home/user/ host:/home/user/.Backup/;
 rm /home/user/.Backup/files.txt;
