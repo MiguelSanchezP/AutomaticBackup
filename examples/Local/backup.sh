013b90ecb17a9dafee8eacf83a41cd3930848cd9
miguelsanchezp <miguelsanchez1703@gmail.com>
2020-10-25 12:21:43 +0100

    Updated examples

--- examples/Local/backup.sh
+++ examples/Local/backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. /home/user/.bashrc;
 find /home/user/folder/ -newermt `date +%F -d today` >> /home/user/.Backup/files.txt;
 sed -i -e 's/\/home\/user\///g' /home/user/.Backup/files.txt;
 rsync -au --files-from=/home/user/.Backup/files.txt /home/user/ host:/home/user/.Backup/;
