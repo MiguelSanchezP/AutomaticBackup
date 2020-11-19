d5fd5292407d32259eec5a2d87a157dbd25918bc
Miguel Sánchez <miguelsanchez1703@gmail.com>
2020-11-19 09:27:55 +0100

    Add variable to handle date and updated example

--- LocalScriptGenerator.sh
+++ LocalScriptGenerator.sh
@@ -6,11 +6,12 @@ echo "#!/bin/bash" >> backup.sh;
 echo ". /home/"$USER"/.bashrc" >> backup.sh;
 read -p "Write the remote username in where to store files: " ruser;
 read -p "Write the domain in where to copy the files: " host;
+echo -e "date=\`ssh -t "$ruser"@"$host" cat /home/"$ruser"/.Logs/.date.txt\`;" >> backup.sh;
 answer="y";
 while [ $answer = "y" ]
 	do
 		read -p "Write full path of directory to be backed up: " dir;
-		echo -e "find "$dir" -newermt \`ssh -t "$ruser"@"$host" cat /home/"$ruser"/.Logs/.date.txt\` >> /home/"$USER"/.Backup/files.txt;" >> backup.sh;
+		echo -e "find "$dir" -newermt \$date >> /home/"$USER"/.Backup/files.txt;" >> backup.sh;
 		read -p "Do you wish to add anohter directory [y/n]? " answer;
 	done
 echo "sed -i -e 's/\/home\/"$USER"\///g' /home/"$USER"/.Backup/files.txt;" >> backup.sh;
--- examples/Local/backup.sh
+++ examples/Local/backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 . /home/username/.bashrc
-find /path/to/directory/ -newermt `ssh -t usernameremote@domain cat /home/usernameremote/.Logs/.date.txt` >> /home/username/.Backup/files.txt;
-find /path/to/another/directory/ -newermt `date +%F -d today` >> /home/username/.Backup/files.txt;
+date=`ssh -t remoteusername@domain cat /home/remoteusername/.Logs/.date.txt`;
+find /path/to/directory/ -newermt $date >> /home/username/.Backup/files.txt;
+find /path/to/another/directory/ -newermt $date >> /home/username/.Backup/files.txt;
 sed -i -e 's/\/home\/username\///g' /home/username/.Backup/files.txt;
-rsync -au --files-from=/home/username/.Backup/files.txt /home/username/ usernameremote@domain:/home/usernameremote/.Backup/;
-ssh -t usernameremote@domain 'date +%F -d today > /home/usernameremote/.Logs/.date.txt';
+rsync -au --files-from=/home/username/.Backup/files.txt /home/username/ remoteusername@domain:/home/remoteusername/.Backup/;
+ssh -t remoteusername@domain 'date +%F -d today > /home/remoteusername/.Logs/.date.txt';
 rm /home/username/.Backup/files.txt;
